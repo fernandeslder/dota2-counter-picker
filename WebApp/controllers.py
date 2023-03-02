5e418a35f89047b0ffd104c5cb6e5deb529cbb1b
Leander Fernandes <fernandeslder@gmail.com>
2023-03-02 09:32:17 +0530

    Simple flask webapp structure

---
+++ WebApp/controllers.py
@@ -0,0 +1,8 @@
+from flask import jsonify
+from app import app
+import services
+
+@app.route('/')
+def index():
+    message = services.get_hello_message()
+    return jsonify({'message': message})
