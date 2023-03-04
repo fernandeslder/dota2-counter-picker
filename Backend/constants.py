8c1b944e015de03305f28eb4d16693a94116cffa
Leander Fernandes <fernandeslder@gmail.com>
2023-03-04 12:33:50 +0530

    added function to fetch all hero list and dump names

--- Backend/constants.py
+++ Backend/constants.py
@@ -1,7 +1,8 @@
 # CONSTANTS
 
 DBUFF_HERO_URL = "https://www.dotabuff.com/heroes"
-HEADER_USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+HEADER_USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                          "Chrome/58.0.3029.110 Safari/537.3 "
 HEADER_USER_AGENT = "User-Agent"
 
 # dotabuff filters
--- Backend/services.py
+++ Backend/services.py
@@ -1,5 +1,10 @@
 import pickle
 import constants
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import pandas as pd
 
 
 def reload_data():
@@ -38,6 +43,26 @@ def final_data(hero_list):
     df_avg_wr = average_win_rate(hero_list)
     final_data_dict['Cumulative Advantage'] = df_sum_adv.to_dict()
     final_data_dict['Average Enemy WR'] = df_avg_wr.to_dict()
+
+
+# def sync_data():
+
+
+def sync_hero_names():
+    headers = {constants.HEADER_USER_AGENT: constants.HEADER_USER_AGENT_VALUE}
+
+    req = requests.get(constants.DBUFF_HERO_URL, headers=headers)
+    soup = BeautifulSoup(req.content, "html.parser")
+    hero_links = soup.find_all("a", href=re.compile(r"/heroes/.*"))
+    heroes = []
+    for link in hero_links:
+        if link.find("div", class_="hero"):
+            heroes.append({"hero_name": link.text.strip(), "link_name": link['href'][8:]})
+
+    all_hero_names_list = [hero_dict['hero_name'] for hero_dict in heroes]
+    with open(f'{constants.DATA_PATH}/all_hero_names_list.json', 'w') as hero_names_out:
+        hero_names_out.write(json.dumps(all_hero_names_list))
+    return heroes
 
 
 with open(f'{constants.DATA_PATH}/dbuff_adv_data.pkl', 'rb') as file_load:
