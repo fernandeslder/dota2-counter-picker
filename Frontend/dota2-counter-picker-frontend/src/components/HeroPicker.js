9aa5017a161a8c4f7aa21f4d71d17d2c30de6946
Leander Fernandes <fernandeslder@gmail.com>
2023-03-11 01:45:17 +0530

    Made hero selection grid horizontal

--- Frontend/dota2-counter-picker-frontend/src/components/HeroPicker.js
+++ Frontend/dota2-counter-picker-frontend/src/components/HeroPicker.js
@@ -5,6 +5,7 @@ import "./HeroPicker.css";
 function HeroPicker() {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedHeroes, setSelectedHeroes] = useState([]);
+  const [heroesSelected, setHeroesSelected] = useState(false);
 
   const handleSearchTermChange = (event) => {
     setSearchTerm(event.target.value);
@@ -13,9 +14,16 @@ function HeroPicker() {
   const handleHeroClick = (hero) => {
     if (selectedHeroes.includes(hero)) {
       setSelectedHeroes(selectedHeroes.filter((h) => h !== hero));
+      setHeroesSelected(selectedHeroes.length > 1);
     } else if (selectedHeroes.length < 5) {
       setSelectedHeroes([...selectedHeroes, hero]);
+      setHeroesSelected(true);
     }
+  };
+
+  const clearSelectedHeroes = () => {
+    setSelectedHeroes([]);
+    setHeroesSelected(false);
   };
 
   const filteredHeroes = heroList.filter((hero) =>
@@ -31,15 +39,19 @@ function HeroPicker() {
           value={searchTerm}
           onChange={handleSearchTermChange}
         />
+        {heroesSelected && (
+          <button className="clear-button" onClick={clearSelectedHeroes}>
+            Clear All Selected Heroes
+          </button>
+        )}
       </div>
       <div className="hero-list" onWheel={(e) => e.currentTarget.scrollBy(e.deltaY, 0)}>
         {filteredHeroes.map((hero) => (
           <div
             key={hero}
             onClick={() => handleHeroClick(hero)}
-            className={`hero-item ${
-              selectedHeroes.includes(hero) ? "selected" : ""
-            }`}
+            className={`hero-item ${selectedHeroes.includes(hero) ? "selected" : ""
+              }`}
           >
             {hero}
           </div>
